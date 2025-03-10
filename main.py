183edc0c17ac113feacb97042033932af931617a
Salimata12lol <salimatadiaralukau@gmail.com>
2025-03-10 11:25:38 +0000

    Code2college

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-print( Welcome to Sali boutique)
-name = input("what is your name?")
-age = input("Hello" + name + ", how can i help you today")
-Response1 = input("Are you sure you don't need help?")
-Repsonse2 = input( " okay let me know when you do okay bye.")
+def resturantinfo(resturant_tables):
+    #try and find out any information for the first row
+    https://docs.python.org/3/library/stdtypes.html#string-methods
+    resturantinfo=
+    for, table (resturant_tables[0][1:]):
+        capacity=int(table.split[1].strip) # Extract capacity (e.g.,2)
+        resturantinfo[i+1]={'id': resturantinfo_id, capacity:capacity}
+    return resturantinfo
