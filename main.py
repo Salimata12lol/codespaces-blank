56839e9a4ef789a0e375a1a3c2729235ec7d7884
Salimata12lol <salimatadiaralukau@gmail.com>
2025-05-01 23:21:14 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,39 @@
-def resturantinfo(resturant_tables):
-    #try and find out any information for the first row
-    https://docs.python.org/3/library/stdtypes.html#string-methods
-    resturantinfo=
-    for, table (resturant_tables[0][1:]):
-        capacity=int(table.split[1].strip) # Extract capacity (e.g.,2)
-        resturantinfo[i+1]={'id': resturantinfo_id, capacity:capacity}
-    return resturantinfo
+import sqlite3
+
+# Connect to (or create) the database
+conn = sqlite3.connect("banking_system.db")
+cursor = conn.cursor()
+
+# Create Accounts table
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS Accounts (
+    account_id INTEGER PRIMARY KEY ,
+    name TEXT NOT NULL,
+    account_number TEXT UNIQUE NOT NULL,
+    account_type TEXT NOT NULL,
+    balance REAL DEFAULT,
+    status TEXT DEFAULT 'Active'
+);
+""")
+
+# Create Transactions table
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS Transactions (
+    transaction_id INTEGER PRIMARY KEY,
+    account_number TEXT NOT NULL,
+    transaction_type TEXT NOT NULL,
+    amount REAL NOT NULL,
+    transaction_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (account_number) REFERENCES Accounts(account_number)
+);
+""")
+
+# Commit changes and close connection
+conn.commit()
+conn.close()
+
+print(" Database and tables created successfully.")
+ 
+
+
+
